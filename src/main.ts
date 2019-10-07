6260dc60e0e2f84997bf44b195efefdcb3e63b24
Alexander Belokon <ikatamarinaki@gmail.com>
2019-10-07 14:07:59 +0300

    back to normal

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,9 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import helmet from 'helmet'
-import fs from 'fs'
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./secrets/private-key.pem'),
-    cert: fs.readFileSync('./secrets/public-certificate.pem'),
-  }
-  const app = await NestFactory.create(AppModule, { httpsOptions })
+  const app = await NestFactory.create(AppModule)
   app.use(helmet())
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())

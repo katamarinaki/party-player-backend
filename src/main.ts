1d7ada7737a6d530b07bf0f8508390be9ccc2419
Alexander Belokon <ikatamarinaki@gmail.com>
2019-10-07 14:06:30 +0300

    ?

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import helmet from 'helmet'
+import fs from 'fs'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  }
+  const app = await NestFactory.create(AppModule, { httpsOptions })
   app.use(helmet())
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
